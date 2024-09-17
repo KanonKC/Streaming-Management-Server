9237d95fad0d0ba4448898dfbc2f6baa45dce8ab
KanonKC <kanonkce@gmail.com>
2024-09-17 12:21:26 +0700

    Deadhard trigger for M4

---
+++ src/triggers/dead-hard-m4.py
@@ -0,0 +1,24 @@
+import win32api, win32con
+from time import sleep
+from pynput.mouse import Button, Controller
+
+mouse = Controller()
+
+def pressEBetweenKey(key):
+    win32api.keybd_event(key, 0, 0, 0)
+    sleep(0.1)
+    mouse.click(Button.x1)
+    sleep(0.1)
+    win32api.keybd_event(key, 0, win32con.KEYEVENTF_KEYUP, 0)
+
+# Press middle mouse
+win32api.mouse_event(win32con.MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, 0)
+
+# Press W A S D
+pressEBetweenKey(0x57)
+pressEBetweenKey(0x41)
+pressEBetweenKey(0x53)
+pressEBetweenKey(0x44)
+
+# Release middle mouse
+win32api.mouse_event(win32con.MOUSEEVENTF_MIDDLEUP, 0, 0, 0, 0)
