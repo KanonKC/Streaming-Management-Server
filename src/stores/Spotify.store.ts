42ca52fc1ab2c01b37cda88003e1abc6cfc8998f
KanonKC <kanonkce@gmail.com>
2024-10-04 22:06:58 +0700

    Implement auto-refresh token flow

--- src/stores/Spotify.store.ts
+++ src/stores/Spotify.store.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../database/prisma";
+import { getRefreshToken } from "../services/Spotify.service";
 import { SpotifyAuthorization } from "../types/Spotify.type";
 
 class SpotifyStore {
@@ -6,8 +7,7 @@ class SpotifyStore {
     namespace = 'main';
     accessToken: string | null = null;
     refreshToken: string | null = null;
-    expires: number | null = null;
-
+    expires: Date | null = null;
 
     constructor() {}
 
@@ -16,9 +16,14 @@ class SpotifyStore {
             where: { namespace: this.namespace },
         });
 
+        if ((!storage || !storage?.spotifyTokenExpires || storage?.spotifyTokenExpires < new Date()) && this.refreshToken) {
+            const newRefreshToken = await getRefreshToken(this.refreshToken);
+            await this.setToken(newRefreshToken.data);
+        }
+
         this.accessToken = storage?.spotifyAccessToken || null;
         this.refreshToken = storage?.spotifyRefreshToken || null;
-        this.expires = storage?.spotifyTokenExpires ? storage.spotifyTokenExpires.getTime() : null;
+        this.expires = storage?.spotifyTokenExpires ? storage.spotifyTokenExpires : null;
 
         return {
             accessToken: this.accessToken,
@@ -36,11 +41,12 @@ class SpotifyStore {
     }
 
     async setToken(spotifyAuthorization: SpotifyAuthorization) {
-        await prisma.storage.upsert({
+        const result = await prisma.storage.upsert({
             where: { namespace: this.namespace },
             update: {
                 spotifyAccessToken: spotifyAuthorization.access_token,
                 spotifyRefreshToken: spotifyAuthorization.refresh_token,
+                spotifyTokenExpires: new Date(Date.now() + spotifyAuthorization.expires_in * 1000),
             },
             create: {
                 namespace: this.namespace,
@@ -50,7 +56,15 @@ class SpotifyStore {
             },
         });
 
-        return this.loadToken();
+        this.accessToken = result.spotifyAccessToken;
+        this.refreshToken = result.spotifyRefreshToken;
+        this.expires = result.spotifyTokenExpires;
+
+        return {
+            accessToken: this.accessToken,
+            refreshToken: this.refreshToken,
+            expires: this.expires,
+        }
     }
 }
 
