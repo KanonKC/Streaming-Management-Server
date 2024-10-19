539ff4ddd61bc229869b1600a6c5156b57faedf5
KanonKC <kanonkce@gmail.com>
2024-10-19 12:44:02 +0700

    Add new Tarot card voices

--- src/constants/Tarot.constant.ts
+++ src/constants/Tarot.constant.ts
@@ -144,7 +144,9 @@ export const MajorCards: TarotCard[] = [
         name: "The Tower",
         description: "การเปลี่ยนแปลงกะทันหัน การทะเลาะเบาะแว้ง ต้องเตรียมใจให้ดี เพราะอาจเจอเรื่องไม่คาดฝัน ได้รับข่าวสารที่ไม่ทันได้เตรียมใจ หรือเกิดเรื่องบาดหมางใหญ่โตกับคนรอบตัว",
         sounds: [
-            { filename: "16.mp3", voiceActor: "", voiceActorTwitchId: "849610457" }
+            // { filename: "blank-10.mp3", voiceActor: "", voiceActorTwitchId: "849610457" }
+            { filename: "16_1.mp3", voiceActor: "", voiceActorTwitchId: "849610457" },
+            { filename: "16_2.mp3", voiceActor: "", voiceActorTwitchId: "849610457" },
         ],
     },
     {
