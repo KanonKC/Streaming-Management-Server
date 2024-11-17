e0fb4f4c7b6aabcd7ad7d05eca5906259c456718
KanonKC <kanonkce@gmail.com>
2024-11-17 14:54:30 +0700

    Update Spotify type

--- src/types/Spotify.type.ts
+++ src/types/Spotify.type.ts
@@ -1,38 +1,76 @@
 export interface SpotifyAuthorization {
-    access_token: string
-    token_type: string
-    expires_in: number
-    refresh_token: string
-    scope: string
+	access_token: string;
+	token_type: string;
+	expires_in: number;
+	refresh_token: string;
+	scope: string;
 }
 
 export interface AddItemToPlaybackQueuePayload {
-    uri: string
-    device_id?: string
+	uri: string;
+	device_id?: string;
 }
 
 export interface SpotifyArtist {
-    name: string
-    uri: string
+	external_urls: { spotify: string };
+    href: string;
+    id: string;
+    name: string;
+    type: "artist";
+    uri: string;
+}
+
+export interface SpotifyAlbum {
+	album_type: string;
+	total_tracks: number;
+	available_markets: string[];
+	external_urls: { spotify: string };
+	href: string;
+	id: string;
+	images: {
+        url: string;
+        height: number;
+        width: number;
+    }[];
+	name: string;
+	release_date: string;
+	release_date_precision: string;
+	restrictions: { reason: string };
+	type: string;
+	uri: string;
+	artists: SpotifyArtist[];
 }
 
 export interface SpotifyTrack {
-    artists: SpotifyArtist[]
-    name: string
-    uri: string
-    href: string
-    external_urls: {
-        spotify: string
-    }
+	album: SpotifyAlbum;
+	artists: SpotifyArtist[];
+	available_markets: string[];
+	disc_number: number;
+	duration_ms: number;
+	explicit: boolean;
+	external_ids: { isrc: string; ean: string; upc: string };
+	external_urls: { spotify: string };
+	href: string;
+	id: string;
+	is_playable: boolean;
+	linked_from: {};
+	restrictions: { reason: string };
+	name: string;
+	popularity: number;
+	preview_url: string;
+	track_number: number;
+	type: "track";
+	uri: string;
+	is_local: boolean;
 }
 
 export interface SpotifySearchResult {
-    tracks: {
-        items: SpotifyTrack[]
-    }
+	tracks: {
+		items: SpotifyTrack[];
+	};
 }
 
 export interface SpotifyUserQueue {
-    currently_playing: SpotifyTrack | null
-    queue: SpotifyTrack[]
+	currently_playing: SpotifyTrack | null;
+	queue: SpotifyTrack[];
 }
